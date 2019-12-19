9189bdc02d49f62fd2f00b98c8c5df07d30ddb76
schneefux <schneefux+commit@schneefux.xyz>
2019-12-19 15:10:28 +0100

    Clear unused seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -2,47 +2,6 @@ import { Photon } from '@prisma/photon'
 const photon = new Photon()
 
 async function main() {
-  const memeBonus = await photon.trumpAttributes.create({
-    data: {
-      name: 'Meme Bonus',
-    }
-  })
-
-  const impeachmentPack = await photon.trumpPacks.create({
-    data: {
-      name: 'Impeachment Deck',
-      cards: {
-        create: [{
-          name: 'Bernie Sanders',
-          description: 'Should-a been bernie',
-          attributeValues: {
-            create: [{
-              attribute: {
-                connect: {
-                  id: memeBonus.id,
-                }
-              },
-              value: 99,
-            }]
-          }
-        }]
-      },
-    },
-  })
-
-  const timo = await photon.users.create({
-    data: {
-      email: 'timo@project-cards.io',
-      name: 'Timo',
-      trumpPacks: {
-        connect: [{
-          id: impeachmentPack.id,
-        }],
-      }
-    },
-  })
-
-  console.log({ timo })
 }
 
 main()
