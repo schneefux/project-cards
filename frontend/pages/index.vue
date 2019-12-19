af52626d7d94fb71c09d25d144fe8bf21ba9fad8
schneefux <schneefux+commit@schneefux.xyz>
2019-12-19 16:13:28 +0100

    Formatting with prettier

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,26 +1,25 @@
 <template>
-  <v-layout row>
-    <v-flex xs12>
-      Packs: {{ trumpPacks }}
-    </v-flex>
-    <v-flex xs12>
-      Games: {{ trumpGames }}
-    </v-flex>
-    <v-flex xs12>
-      <v-btn @click="addUser">
-        Neuen Benutzer anlegen
-      </v-btn>
-      <v-btn @click="addPack">
-        Neues Pack anlegen
-      </v-btn>
-      <v-btn @click="startTrumpGame">
-        Neues Spiel starten
-      </v-btn>
-    </v-flex>
+  <div>
+    <v-layout row>
+      <v-flex xs12> Packs: {{ trumpPacks }} </v-flex>
+      <v-flex xs12> Games: {{ trumpGames }} </v-flex>
+      <v-flex xs12>
+        <v-btn @click="addUser">
+          Neuen Benutzer anlegen
+        </v-btn>
+        <v-btn @click="addPack">
+          Neues Pack anlegen
+        </v-btn>
+        <v-btn @click="startTrumpGame">
+          Neues Spiel starten
+        </v-btn>
+      </v-flex>
+    </v-layout>
+
     <button class="shadow bg-blue-500 px-4 py-2 text-white hover:bg-blue-400">
       tailwind button test
     </button>
-  </v-layout>
+  </div>
 </template>
 
 <script>
@@ -29,30 +28,36 @@ import gql from 'graphql-tag'
 export default {
   apollo: {
     trumpPacks: {
-      query: gql`query {
-        trumpPacks {
-          name
+      query: gql`
+        query {
+          trumpPacks {
+            name
+          }
         }
-      }`,
+      `
     },
     trumpGames: {
-      query: gql`query {
-        trumpGames {
-          id
-        }
-      }`,
-      subscribeToMore: {
-        document: gql`subscription trumpGames {
-          createdTrumpGame {
+      query: gql`
+        query {
+          trumpGames {
             id
           }
-        }`,
+        }
+      `,
+      subscribeToMore: {
+        document: gql`
+          subscription trumpGames {
+            createdTrumpGame {
+              id
+            }
+          }
+        `,
         updateQuery: (previousResult, { subscriptionData }) => {
           return {
             trumpGames: [
               ...previousResult.trumpGames,
-              subscriptionData.data.createdTrumpGame,
-            ],
+              subscriptionData.data.createdTrumpGame
+            ]
           }
         }
       }
@@ -62,30 +67,28 @@ export default {
     return {
       user: undefined,
       player: undefined,
-      pack: undefined,
+      pack: undefined
     }
   },
   methods: {
     async addUser() {
       const response = await this.$apollo.mutate({
-        mutation: gql`mutation ($name: String!, $email: String!) {
-          createOneUser (data: {
-            name: $name,
-            email: $email,
-            trumpPlayer: {
-              create: {}
-            }
-          }) {
-            id
-            trumpPlayer {
+        mutation: gql`
+          mutation($name: String!, $email: String!) {
+            createOneUser(
+              data: { name: $name, email: $email, trumpPlayer: { create: {} } }
+            ) {
               id
+              trumpPlayer {
+                id
+              }
             }
           }
-        }`,
+        `,
         variables: {
           name: 'random name ' + Math.random(),
-          email: 'random@' + Math.random(),
-        },
+          email: 'random@' + Math.random()
+        }
       })
 
       this.user = response.data.createOneUser.id
@@ -93,38 +96,36 @@ export default {
     },
     async addPack() {
       const response = await this.$apollo.mutate({
-        mutation: gql`mutation ($name: String!) {
-          createOneTrumpPack (data: {
-            name: $name,
-          }) {
-            id
+        mutation: gql`
+          mutation($name: String!) {
+            createOneTrumpPack(data: { name: $name }) {
+              id
+            }
           }
-        }`,
+        `,
         variables: {
-          name: 'random pack ' + Math.random(),
-        },
+          name: 'random pack ' + Math.random()
+        }
       })
 
       this.pack = response.data.createOneTrumpPack.id
     },
     async startTrumpGame() {
       await this.$apollo.mutate({
-        mutation: gql`mutation ($player1: String!, $player2: String!, $pack: String!) {
-          startTrumpGame (
-            player1: $player1,
-            player2: $player2,
-            pack: $pack,
-          ) {
-            id
+        mutation: gql`
+          mutation($player1: String!, $player2: String!, $pack: String!) {
+            startTrumpGame(player1: $player1, player2: $player2, pack: $pack) {
+              id
+            }
           }
-        }`,
+        `,
         variables: {
           player1: this.player,
           player2: this.player,
-          pack: this.pack,
-        },
+          pack: this.pack
+        }
       })
     }
-  },
+  }
 }
 </script>
