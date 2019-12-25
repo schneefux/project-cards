34ed0677808ffe75dcdfa88f209d79000138658b
schneefux <schneefux+commit@schneefux.xyz>
2019-12-25 15:54:32 +0100

    Add username to header

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -5,13 +5,27 @@
     >
       <div class="flex-shrink-0 pt-3 md:py-0 w-full md:w-auto">
         <nuxt-link to="/" class="font-semibold text-xl text-white tracking-tighter">Title Goes Here</nuxt-link>
+        <div class="md:hidden float-right mt-1">
+          <nuxt-link
+            v-show="me != undefined"
+            to="/login"
+            class="px-2 py-1 border border-primary-200 rounded-sm text-primary-200 hover:bg-primary-600"
+          >{{ me.name }}</nuxt-link>
+        </div>
       </div>
       <div class="w-full md:w-auto">
         <div class="overflow-x-auto overflow-y-hidden scrolling-touch whitespace-no-wrap">
           <div class="py-2 md:py-0">
             <nuxt-link to="/" class="nav-link">Home</nuxt-link>
-            <nuxt-link to="/login" class="nav-link">Login</nuxt-link>
-            <nuxt-link to="/packs" class="nav-link">Packs</nuxt-link>
+            <nuxt-link v-show="me != undefined" to="/packs" class="nav-link">Packs</nuxt-link>
+            <nuxt-link v-show="me == undefined" to="/login" class="nav-link">Login</nuxt-link>
+            <div class="hidden md:inline-block">
+              <nuxt-link
+                v-show="me != undefined"
+                to="/login"
+                class="inline font-medium border-b border-primary-200 text-primary-200 hover:text-primary-100"
+              >{{ me.name }}</nuxt-link>
+            </div>
           </div>
         </div>
       </div>
@@ -26,8 +40,28 @@
   </div>
 </template>
 
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    me: gql`
+      query {
+        me {
+          name
+        }
+      }
+    `
+  }
+}
+</script>
+
 <style scoped>
 .nav-link {
-  @apply inline-block mr-4 text-primary-100 border-b border-primary-100 font-medium;
+  @apply inline mr-4 text-primary-100 border-b border-primary-100 font-medium;
+}
+
+.nav-link:hover {
+  @apply text-primary-200;
 }
 </style>
