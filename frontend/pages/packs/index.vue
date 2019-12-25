8dc5e262cb962934517fa967b00ea2b6cf8464c1
schneefux <schneefux+commit@schneefux.xyz>
2019-12-25 15:17:39 +0100

    Add 'stack' effect to packs

--- frontend/pages/packs/index.vue
+++ frontend/pages/packs/index.vue
@@ -13,15 +13,19 @@
         :to="`/packs/${pack.id}`"
         class="playingcard playingcard--lg playingcard--interactive"
       >
-        <div class="playingcard__container leading-loose">
-          <p class="playingcard__title text-primary-500 underline">{{ pack.name }}</p>
-          <p>Author: {{ pack.author.name }}</p>
-          <p>Description: {{ pack.description }}</p>
-          <div class="playingcard__attributes">
-            <p class="text-left mt-2 pt-1 mb-1 border-t">Attributes:</p>
-            <ul class="ml-6">
-              <li v-for="attribute in pack.attributes" :key="attribute.id">{{ attribute.name }}</li>
-            </ul>
+        <div class="playingcard__container">
+          <div class="playingcard__container">
+            <div class="playingcard__container leading-loose">
+              <p class="playingcard__title text-primary-500 underline">{{ pack.name }}</p>
+              <p>Author: {{ pack.author.name }}</p>
+              <p>Description: {{ pack.description }}</p>
+              <div class="playingcard__attributes">
+                <p class="text-left mt-2 pt-1 mb-1 border-t">Attributes:</p>
+                <ul class="ml-6">
+                  <li v-for="attribute in pack.attributes" :key="attribute.id">{{ attribute.name }}</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </nuxt-link>
