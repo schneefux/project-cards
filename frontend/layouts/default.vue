d1f0998d6c1bf571a3a8388ae09e1ba293dc1e77
schneefux <schneefux+commit@schneefux.xyz>
2019-12-30 00:00:19 +0100

    Highlight active link in navigation

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,24 +4,38 @@
       class="bg-white border-b-gradient px-4 md:pt-4 flex justify-between items-center flex-wrap sticky top-0 md:static z-50"
     >
       <div class="flex-shrink-0 pt-3 md:py-0 w-full md:w-auto">
-        <nuxt-link to="/" class="font-bold text-xl tracking-tighter nav-link">[Project Name]</nuxt-link>
+        <nuxt-link to="/" class="font-bold text-xl tracking-tighter nav-link"
+          >[Project Name]</nuxt-link
+        >
         <div class="md:hidden float-right mt-1">
           <nuxt-link
             v-if="me != undefined"
             to="/login"
             class="px-2 py-1 border rounded-sm nav-link"
-          >{{ me.name }}</nuxt-link>
+            >{{ me.name }}</nuxt-link
+          >
         </div>
       </div>
-      <div class="w-full md:w-auto">
-        <div class="overflow-x-auto overflow-y-hidden scrolling-touch whitespace-no-wrap">
+      <div class="menu w-full md:w-auto">
+        <div
+          class="overflow-x-auto overflow-y-hidden scrolling-touch whitespace-no-wrap"
+        >
           <div class="py-2 md:py-0 font-medium">
-            <nuxt-link to="/" class="inline mr-4 font-medium nav-link">Home</nuxt-link>
-            <nuxt-link v-show="me != undefined" to="/packs" class="inline mr-4 nav-link">Packs</nuxt-link>
-            <nuxt-link v-show="me != undefined" to="/games" class="inline mr-4 nav-link">Live Games</nuxt-link>
-            <nuxt-link v-show="me == undefined" to="/login" class="inline mr-4 nav-link">Login</nuxt-link>
+            <nuxt-link
+              v-for="link in links"
+              :key="link.to"
+              :to="link.to"
+              :exact="link.exact"
+              class="inline mr-4 font-medium nav-link"
+              >{{ link.title }}</nuxt-link
+            >
             <div class="hidden md:inline-block">
-              <nuxt-link v-if="me != undefined" to="/login" class="inline nav-link">{{ me.name }}</nuxt-link>
+              <nuxt-link
+                v-if="me != undefined"
+                to="/login"
+                class="inline nav-link"
+                >{{ me.name }}</nuxt-link
+              >
             </div>
           </div>
         </div>
@@ -30,7 +44,9 @@
 
     <nuxt class="flex-grow" />
 
-    <footer class="bg-gray-100 py-2 text-sm text-gray-600 text-center leading-normal">
+    <footer
+      class="bg-gray-100 py-2 text-sm text-gray-600 text-center leading-normal"
+    >
       <p>Copyright goes here</p>
       <p class="text-xs leading-tight">Disclaimer goes here</p>
     </footer>
@@ -49,17 +65,62 @@ export default {
         }
       }
     `
+  },
+  computed: {
+    loggedIn() {
+      return this.me != undefined
+    },
+    links() {
+      if (this.loggedIn) {
+        return [
+          {
+            to: '/',
+            title: 'Home',
+            exact: true
+          },
+          {
+            to: '/packs',
+            title: 'Packs'
+          },
+          {
+            to: '/games',
+            title: 'Live Games'
+          }
+        ]
+      } else {
+        return [
+          {
+            to: '/',
+            title: 'Home'
+          },
+          {
+            to: '/login',
+            title: 'Login'
+          }
+        ]
+      }
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .nav-link {
   @apply text-gray-700;
+
+  &:hover {
+    @apply text-primary-600;
+  }
 }
 
-.nav-link:hover {
-  @apply text-primary-600;
+.menu {
+  .nuxt-link-active {
+    @apply text-secondary-600;
+
+    &:hover {
+      @apply text-primary-600;
+    }
+  }
 }
 
 .border-b-gradient:after {
