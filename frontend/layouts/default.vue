d2f26cc94ce497e55b5909cc56d961a78868bf54
schneefux <schneefux+commit@schneefux.xyz>
2019-12-30 14:54:52 +0100

    Fix home nav link highlight on mobile

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -91,7 +91,8 @@ export default {
         return [
           {
             to: '/',
-            title: 'Home'
+            title: 'Home',
+            exact: true
           },
           {
             to: '/login',
