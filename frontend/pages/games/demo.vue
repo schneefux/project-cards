92aee77570daf556fa749c70cb1853efdfe445e4
schneefux <schneefux+commit@schneefux.xyz>
2020-01-04 18:36:51 +0100

    GameUI: Replace magic numbers by constants

--- frontend/pages/games/demo.vue
+++ frontend/pages/games/demo.vue
@@ -2,28 +2,23 @@
   <div class="container container--page">
     <h1 class="page-heading">Game Client Demo</h1>
 
-    <div
-      class="w-full"
-      v-for="side in 2"
-      :key="side"
-      :class="{ 'transform-flip-y': side == 1 }"
-    >
+    <div class="w-full" v-for="side in 2" :key="side" :class="{ 'transform-flip-y': side == 1 }">
       <div
         class="relative w-full h-40 rounded-b-lg"
         :class="{ 'bg-secondary-500': side == 1, 'bg-primary-500': side == 2 }"
       >
         <template v-if="side == 2">
           <div
-            v-for="n in 2"
+            v-for="n in cardsPrice"
             :key="n"
             class="playingcard__container"
             :style="
               `
               position: absolute;
-              left: calc(50% - 3rem + ${(n - 1 + 0.5 - cardsPrice / 2) * 7}rem);
-              top: -5rem;
-              width: 6rem;
-              height: 9rem;
+              left: calc(50% - ${CENTER_CARD_W / 2}rem - ${(n - 1 + 0.5 - cardsPrice / 2) * CENTER_CARD_W * 1.5}rem);
+              width: ${CENTER_CARD_W}rem;
+              top: -${CENTER_CARD_W*CARD_RATIO / 2}rem;
+              height: ${CENTER_CARD_W*CARD_RATIO}rem;
               `
             "
           ></div>
@@ -32,10 +27,10 @@
       <div
         class="border border-gray-700 relative mx-auto rounded-full"
         :class="{ 'bg-secondary-500': side == 1, 'bg-primary-500': side == 2 }"
-        style="
-        width: 20rem;
-        height: 8rem;
-        "
+        :style="`
+        width: ${HAND_SPACE_W}rem;
+        height: ${HAND_SPACE_H}rem;
+        `"
       >
         <div
           v-for="n in cardsRed"
@@ -43,30 +38,26 @@
           :style="
             `;
             position: absolute;
-            top: -4rem;
-            left: calc(50% - 2rem);
-            width: 4rem;
-            height: 6rem;
+            top: -5rem;
+            left: calc(50% - ${HAND_CARD_W / 2}rem);
+            width: ${HAND_CARD_W}rem;
+            height: ${HAND_CARD_W*CARD_RATIO}rem;
             transform-origin: center bottom; \
             transform: \
-              translateX(${(n - 1 + 0.5 - cardsRed / 2) * 4 * 0.5}rem) \
+              translateX(${(n - 1 + 0.5 - cardsRed / 2) * HAND_CARD_W * HAND_SPREAD}rem) \
               rotate(${-30 + ((n - 1 + 0.5) / cardsRed) * 60}deg) \
               translateY(${Math.abs(n - 1 + 0.5 - cardsRed / 2) *
-                6 *
-                0.05}rem) \
+                HAND_CARD_W * CARD_RATIO *
+                HAND_SPREAD * HAND_SPACE_H / HAND_SPACE_W / 2}rem) \
           `
           "
           class="playingcard__container"
-        >
-          {{ n }}
-        </div>
+        >{{ n }}</div>
         <p
           class="text-center w-full absolute font-semibold"
           :class="{ 'transform-flip-y': side == 1 }"
           style="top: 50%"
-        >
-          1234 Punkte
-        </p>
+        >1234 Punkte</p>
       </div>
     </div>
   </div>
@@ -76,21 +67,23 @@
 export default {
   data() {
     return {
-      cardsRed: 8,
-      cardsPrice: 2
+      cardsRed: 6,
+      cardsPrice: 2,
+      CARD_RATIO: 9 / 6,
+      CENTER_CARD_W: 4,
+      HAND_CARD_W: 4,
+      HAND_SPACE_W: 20,
+      HAND_SPACE_H: 8,
+      HAND_SPREAD: 0.5
     }
   },
   computed: {},
-  methods: {},
-  middleware: ['guest']
+  methods: {}
+  //middleware: ['guest']
 }
 </script>
 
 <style scoped lang="scss">
-.absolute\! {
-  position: absolute !important;
-}
-
 .transform-flip-y {
   transform: rotate(180deg);
 }
