307fa7097f60699f9478d2c1cf885539282e670e
schneefux <schneefux+commit@schneefux.xyz>
2020-01-08 12:15:08 +0100

    GameUI: Make cards draggable

--- frontend/pages/games/demo.vue
+++ frontend/pages/games/demo.vue
@@ -15,44 +15,63 @@
             :style="
               `
               position: absolute;
-              left: calc(50% - ${CENTER_CARD_W / 2}rem - ${(n - 1 + 0.5 - cardsPrice / 2) * CENTER_CARD_W * 1.5}rem);
+              left: calc(50% - ${CENTER_CARD_W / 2}rem - ${(n -
+                1 +
+                0.5 -
+                cardsPrice / 2) *
+                CENTER_CARD_W *
+                1.5}rem);
               width: ${CENTER_CARD_W}rem;
-              top: -${CENTER_CARD_W*CARD_RATIO / 2}rem;
-              height: ${CENTER_CARD_W*CARD_RATIO}rem;
+              top: -${(CENTER_CARD_W * CARD_RATIO) / 2}rem;
+              height: ${CENTER_CARD_W * CARD_RATIO}rem;
               `
             "
           ></div>
         </template>
       </div>
       <div
-        class="border border-gray-700 relative mx-auto rounded-full"
+        class="border border-gray-700 relative mx-auto rounded-full flex"
         :class="{ 'bg-secondary-500': side == 1, 'bg-primary-500': side == 2 }"
-        :style="`
+        :style="
+          `
         width: ${HAND_SPACE_W}rem;
         height: ${HAND_SPACE_H}rem;
-        `"
+        `
+        "
       >
-        <div
-          v-for="n in cardsRed"
-          :key="n"
-          :style="
-            `;
-            position: absolute;
-            top: -5rem;
-            left: calc(50% - ${HAND_CARD_W / 2}rem);
-            width: ${HAND_CARD_W}rem;
-            height: ${HAND_CARD_W*CARD_RATIO}rem;
-            transform-origin: center bottom; \
-            transform: \
-              translateX(${(n - 1 + 0.5 - cardsRed / 2) * HAND_CARD_W * HAND_SPREAD}rem) \
-              rotate(${-30 + ((n - 1 + 0.5) / cardsRed) * 60}deg) \
-              translateY(${Math.abs(n - 1 + 0.5 - cardsRed / 2) *
-                HAND_CARD_W * CARD_RATIO *
-                HAND_SPREAD * HAND_SPACE_H / HAND_SPACE_W / 2}rem) \
-          `
-          "
-          class="playingcard__container"
-        >{{ n }}</div>
+        <draggable
+          v-model="cards"
+          :group="`cards-${side}`"
+          :style="`
+            margin-top: -5rem;
+            display: grid;
+            grid-template-columns: repeat(${cards.length * 4}, 1fr);
+          `"
+          class="mx-auto h-full"
+          @start="spread = false"
+          @end="spread = true"
+        >
+          <!-- TODO find an alternative to commenting out spread styles -->
+          <div
+            v-for="(card, index) in cards"
+            :key="`${card}`"
+            :style="
+              `
+              grid-column: auto / span 4;
+              grid-row: 1;
+              width: ${HAND_CARD_W}rem;
+              height: ${HAND_CARD_W * CARD_RATIO}rem;
+              ${spread ? '' : '/*'}
+              transform: rotate(${-30 + ((index + 0.5) / cards.length) * 60}deg);
+              ${spread ? '' : '*/'}
+              ${spread ? '' : '/*'}
+              margin-top: ${Math.abs(index + 0.5 - cards.length / 2) / 2}rem;
+              ${spread ? '' : '*/'}
+            `
+            "
+            class="playingcard__container"
+          >{{ card }}</div>
+        </draggable>
         <p
           class="text-center w-full absolute font-semibold"
           :class="{ 'transform-flip-y': side == 1 }"
@@ -64,18 +83,34 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
   data() {
     return {
-      cardsRed: 6,
+      cards: [
+        'card 1',
+        'card 2',
+        'card 3',
+        'card 4',
+        'card 5',
+        'card 6',
+        'card 7',
+        'card 8',
+        'card 9'
+      ],
       cardsPrice: 2,
       CARD_RATIO: 9 / 6,
       CENTER_CARD_W: 4,
       HAND_CARD_W: 4,
       HAND_SPACE_W: 20,
       HAND_SPACE_H: 8,
-      HAND_SPREAD: 0.5
+      dragging: undefined,
+      spread: true
     }
+  },
+  components: {
+    draggable
   },
   computed: {},
   methods: {}
@@ -86,5 +121,9 @@ export default {
 <style scoped lang="scss">
 .transform-flip-y {
   transform: rotate(180deg);
+}
+
+.sortable-chosen {
+  transform: scale(1.5) !important;
 }
 </style>
