7bc0eb2aae7315369baa39662532baf006663d85
schneefux <schneefux+commit@schneefux.xyz>
2020-01-21 21:11:51 +0100

    Update landing page wording for new learning-based concept

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="py-20 bg-gradient-r-dark-to-secondary">
       <div class="container mx-auto px-6">
-        <h2 class="text-4xl font-bold mb-2 text-white">[Project Name]</h2>
-        <h3 class="text-2xl mb-8 text-gray-100">Design, play and print custom card games.</h3>
+        <h2 class="text-4xl font-bold mb-2 text-white">[Projektname]</h2>
+        <h3 class="text-2xl mb-8 text-gray-100">Designe Lernkarten und spiele sie mit Freunden.</h3>
 
-        <nuxt-link to="/games" class="bg-white hover:bg-secondary-100 button--xl">Play Online</nuxt-link>
+        <nuxt-link to="/games" class="bg-white hover:bg-secondary-100 button--xl">Online spielen</nuxt-link>
       </div>
     </div>
 
@@ -14,8 +14,8 @@
 
       <div class="feature">
         <div class="w-full md:w-1/2">
-          <h4 class="feature__heading">Card Designer</h4>
-          <p class="feature__text">Create Trump-style playing cards with custom images and text.</p>
+          <h4 class="feature__heading">Karten-Designer</h4>
+          <p class="feature__text">Erstelle Lernkarten mit Bildern und Text.</p>
         </div>
         <div class="w-full md:w-1/2">
           <PlayingCardsIllustration />
@@ -27,22 +27,21 @@
           <GameDayIllustration />
         </div>
         <div class="w-full md:w-1/2 pl-10">
-          <h4 class="feature__heading">Online Battles</h4>
-          <p class="feature__text">
-            Challenge friends or strangers and play "Goofenspiel", the Game of
-            Pure Strategy with cards created by you or the community.
-          </p>
+          <h4 class="feature__heading">Online-Spiel</h4>
+          <p
+            class="feature__text"
+          >Fordere Fremde und Freunde heraus. Lernt gemeinsam interessante Fakten über die Welt.</p>
         </div>
       </div>
 
       <div class="feature">
         <div class="w-full md:w-1/2 pl-10">
-          <h4 class="feature__heading">Print at Home</h4>
-          <p class="feature__text">Print card games and play offline.</p>
+          <h4 class="feature__heading">Druck sie aus</h4>
+          <p class="feature__text">Druck deine Kartenspiele aus und lerne überall.</p>
           <div class="block -mt-6 mb-6">
             <span
               class="bg-primary-400 text-white px-2 pt-px pb-1 rounded-full text-xs font-semibold tracking-wide"
-            >coming soon</span>
+            >bald</span>
           </div>
         </div>
         <div class="w-full md:w-1/2">
